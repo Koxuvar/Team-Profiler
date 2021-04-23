d42630bb3fc49d63e504ab45990d8eddd94c97cf
MMVHome <connortsullivan1@gmail.com>
2021-04-23 08:53:51 -0500

    disgusting single function for creating employees but it works so hey  ¯\_(ツ)_/¯

--- index.js
+++ index.js
@@ -1,47 +1,85 @@
+/* ------------------ GLOBAL ------------------ */
+
 const inq = require('inquirer');
 const fs = require('fs');
 const prompts = require('./lib/inquirer/prompts');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const Employee = require('./lib/Employee');
 
-async function App()
+let arrEmps = [];
+
+/* ------------------ Functions ------------------ */
+
+/**
+ * make Employee
+ * Takes an int and prompts user questions based on selection for what kind of employee they will add to the arrEmps array 
+ * 
+ * @param {INT} sel selector for which employee type to add
+ * 1 for Manager,
+ * 2 for Engineer,
+ * 3 for Intern,
+ * Anthing else to quit and build team page;
+ */
+const makeEmployee = (sel) =>
 {
-    let q = 1;
-    while(q !== "done")
+    
+    if(!isNaN(sel))
     {
-        let empToAdd;
-        let arrOfEmps;
-        if(q == 1)
+        const selArr = [{'type' : 'Manager', 'prompt' : 'pManager', 'special' : 'eOfficeNumber'}, {'type' : 'Engineer', 'prompt' : 'pEngineer', 'special' : 'eGithub'}, {'type' : 'Intern', 'prompt' : 'pIntern', 'special' : 'eSchool'}];
+        inq.prompt(prompts[selArr[sel].prompt])
+        .then((data) => 
         {
-            let ans = await inq.prompt(prompts);
-            const genDetails = [ans.eName, ans.eID, ans.eEmail];
-            switch(ans.eSelector)
-            {
-                case('Manager'):
-                    empToAdd = new Manager(...genDetails, ans.eOfficeNumber);
-                    break;
-                case('Engineer'):
-                    empToAdd = new Engineer(...genDetails, ans.eGithub);
-                    break;
-                case('Intern'):
-                    empToAdd = new Intern(...genDetails, ans.eSchool);
-                    break;
-
-                default:
-                    empToAdd = new Employee();
-            }
-
-            arrOfEmps.push(empToAdd);
-
-            arrOfEmps.forEach(element => {
-                console.log(element);
-            });
-            
-            q = "done";
-        }
+            const eType = eval(selArr[sel].type);
+            arrEmps.push(new eType(data.eName, data.eID, data.eEmail, data[selArr[sel].special]));
+            getEmployee();
+        });
     }
+    else
+    {
+        buildTeam();
+    }
+}
+
+/**
+ * !TBD
+ */
+const buildTeam = () =>
+{
+    console.log(arrEmps);
+}
+
+
+/**
+ * asks the user to determine whick employee to add next
+ */
+const getEmployee = () =>
+{
+    inq.prompt(prompts.pGeneric)
+    .then(({eSelector}) => 
+    {
+        if(eSelector == 'Engineer')
+        {
+            makeEmployee(1);
+        }
+        else if(eSelector == 'Intern')
+        {
+            makeEmployee(2);
+        }
+        else
+        {
+            buildTeam();
+        }
+    });
+}
+
+/* ------------------ APP ------------------ */
+/**
+ * starts the process with making a manager right away
+ */
+const App = () =>
+{
+    makeEmployee(0);
 }
 
 App();
